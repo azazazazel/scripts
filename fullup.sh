2e4d2f879afecbff2e7df9ed0226cb54493da040
azazazazel <athenacrawley@gmail.com>
2026-08-27 13:53:15 +0100

    fullup update

--- fullup.sh
+++ fullup.sh
@@ -1,4 +1,20 @@
-# /bin/bash
+#! /bin/bash
 # full updater (apt), written by azazazazel
+if sudo apt-get update | grep -q 'can be upgraded.'; then
+	upgrade=true
+fi
 
-sudo apt update && sudo apt dist-upgrade -y && sudo apt autoremove -y
+if sudo apt-get upgrade -y | grep -q 'autoremove'; then
+	autorm=true
+fi
+
+sudo apt update
+
+if $upgrade; then
+	sudo apt upgrade -y
+fi
+
+if $autorm; then
+	sudo apt autoremove -y
+fi
+
